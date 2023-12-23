76c42aa2d4ac5b075d824b4ec1b0af80b4f997f8
Rita <kapustnikova.rita@bk.ru>
2023-12-23 16:28:20 +0300

    добавлена функция sorted_data

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,12 @@ import json
 def load_data(file_path):
     with open(file_path, encoding="utf-8") as file:
         return json.load(file)
+
+
+def sorted_data(data):
+    executed_list = []
+    for item in data:
+        if item.get("state") == "EXECUTED":
+            executed_list.append(item)
+    sorted_json = sorted(executed_list, key=lambda operation: operation["date"], reverse=True)
+    return sorted_json

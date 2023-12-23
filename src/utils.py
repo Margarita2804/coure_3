4be7928a866dbb01a77b485f9eebb17e78c19eb4
Rita <kapustnikova.rita@bk.ru>
2023-12-23 17:28:32 +0300

    добавлена функция transaction_convert_check

--- src/utils.py
+++ src/utils.py
@@ -20,3 +20,17 @@ def sorted_data(data):
 def date_conversion(data_time):
     convert_date = datetime.fromisoformat(data_time).strftime("%d.%m.%Y")
     return convert_date
+
+
+def transaction_convert_check(data_check):
+    data = data_check.split(' ')
+    numbers_check = data[-1]
+    name_check = ' '.join(data[:-1])
+    if name_check.title() == "Счет":
+        final_check = f"{name_check} **{numbers_check[-4:]}"
+        return final_check
+    else:
+        check = f"{numbers_check[:6]}{'*' * 6}{numbers_check[-4:]}"
+        format_check = ' '.join([check[x:x+4]for x in range(0, 16, 4)])
+        final_check = f"{name_check} {format_check}"
+        return final_check
